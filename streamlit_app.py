b8e37b8bffaf0434521f59d83745b53e4b935d54
CodiMaks <150930184+CodiMaks@users.noreply.github.com>
2024-08-05 23:49:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 cookie_manager = CookieController()
-if not cookie_manager:
-    st.stop()
+time.sleep(2)
+controller.getAll()
 
 # cookie_manager.set('user_id', 'cus_maks123')
 # cookie_manager.get('user_id')
